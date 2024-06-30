b25c297ca14ddc318a9de4894e4d9680c160eef2
Kashish Garg <87192533+Kishu305@users.noreply.github.com>
2024-06-30 16:23:54 +0530

    Update SocialMedia.js

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -6,17 +6,17 @@ const SocialMedia = () => {
   return (
     <div>
       <div className="container mx-auto text-center flex gap-5 justify-center items-center mt-5 -mb-6 text-white ">
-        <a href="https://www.instagram.com/kaustubh.mishra.777/">
+        <a href="https://www.instagram.com/mee_kishuu/">
           <div className="fa-instagram icons text-2xl hover:text-red-600 transition-all duration-300">
             <FaInstagram />
           </div>
         </a>
-        <a href="https://www.linkedin.com/in/kaustubh2701/">
+        <a href="https://www.linkedin.com/in/kashishgarg30/">
           <div className="fa-linkedin icons text-2xl hover:text-red-600 transition-all duration-300">
             <FaLinkedin/>
           </div>
         </a>
-        <a href="https://github.com/KaustubhMishra27">
+        <a href="https://github.com/Kishu305">
           <div className="fa-github icons text-2xl hover:text-red-600 transition-all duration-300">
             <FaGithub/>
           </div>
